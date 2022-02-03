7686a18918e2f50491f9c17261fa920c70b1c9bf
Joonas Hiltunen <joonas.s.jh@gmail.com>
2022-02-03 12:46:58 +0200

    Added possibility to add 3d model with button press. Button is hidden when pressing the model

--- app/src/main/java/com/jhiltunen/simplearapp/MainActivity.kt
+++ app/src/main/java/com/jhiltunen/simplearapp/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.jhiltunen.simplearapp
 
+import android.graphics.Point
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
 import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
     private lateinit var arFrag: ArFragment
     private var viewRenderable: ViewRenderable? = null
+    private var modelRenderable: ModelRenderable? = null
+    private var TAG = "DBG"
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,5 +54,61 @@ class MainActivity : AppCompatActivity() {
             viewNode.select()
         }
 
+        button = findViewById(R.id.btn_add_tree)
+        button.setOnClickListener {
+            add3dObject()
+        }
+
+        // (CC BY 4.0) Donated by Cesium for glTF testing.
+        // https://github.com/KhronosGroup/glTF-Sample-Models/
+        // put gltf, bin, jpg in assets
+
+        ModelRenderable.builder()
+            .setSource(this, Uri.parse("demo.gltf"))
+            .setIsFilamentGltf(true)
+            .setAsyncLoadEnabled(true)
+            .setRegistryId("Avocado")
+            .build()
+            .thenAccept { modelRenderable = it }
+            .exceptionally {
+                Log.e(TAG, "something went wrong ${it.localizedMessage}")
+                null
+            }
+    }
+
+    private fun getScreenCenter(): Point {
+        // find the root view of the activity
+        val vw = findViewById<View>(android.R.id.content)
+        // returns center of the screen as a Point object
+        return Point(vw.width / 2, vw.height / 2)
+    }
+
+    private fun add3dObject() {
+        val frame = arFrag.arSceneView.arFrame
+        if (frame != null && modelRenderable != null) {
+            val pt = getScreenCenter()
+            // get list of HitResult of the given location in the camera view
+            val hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane) {
+                    val anchorNode = AnchorNode(hit!!.createAnchor())
+                    anchorNode.parent = arFrag.arSceneView.scene
+                    val mNode = TransformableNode(arFrag.transformationSystem)
+                    mNode.renderable = modelRenderable
+                    // Default min is 0.75, default max is 1.75.
+                    mNode.scaleController.minScale = 0.05f
+                    mNode.scaleController.maxScale = 2.0f
+                    mNode.localScale = Vector3(0.2f, 0.2f, 0.2f) // scale at 20%
+                    mNode.setOnTapListener { _, _ ->
+                        button.visibility = View.INVISIBLE
+                    }
+                    // scale must be set before setting parent
+                    mNode.parent = anchorNode
+                    mNode.select()
+                    break
+                }
+            }
+        }
     }
 }
